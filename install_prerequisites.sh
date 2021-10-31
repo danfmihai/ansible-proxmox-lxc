230fd11d4a1410a0452d7a7e39b76374d6cc9355
Mihai <danfmihai@hotmail.com>
2021-10-31 16:14:16 +0000

    edited

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
+echo "_________________________"
 echo "Install Ansible:"
+echo "_________________________"
 sudo apt install -y ansible
+echo "_________________________"
 echo "Install python3 and pip:"
+echo "_________________________"
 sudo apt install -y python3-venv python3-pip
+echo "_________________________"
 echo "Install Ansible collection community.general:"
+echo "_________________________"
 ansible-galaxy collection install community.general
+echo "_________________________"
 echo "Install proxmoxer:"
+echo "_________________________"
 sudo pip3 install proxmoxer
